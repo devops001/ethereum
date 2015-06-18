0280e4772a36bf4c9d96e00286b20fde8896b33b
devops001 <devops001@gmail.com>
2015-06-18 07:24:41 -0700

    old update to test contract; ignoring data dir

--- contracts/investedParty.js
+++ contracts/investedParty.js
@@ -4,49 +4,44 @@ contract investedParty {
   struct Investor {
     address addr;
     uint    amount;
+    // TODO: add dates: investedAt, investedUntil
+    // TODO: return investment after investedUntil
   }
 
-  struct Campaign {
-    string32 name;
+  struct Company {
+    string32 dapp;
     address  creator;
-    address  beneficiary;
-    uint     goal;
+    address  wallet;
     uint     amount;
     uint     numInvestors;
-    bool     isGoalMet;
     mapping (uint => Investor) investors;
   }
 
-  uint numCampaigns;
-  mapping (uint => Campain) campaigns;
+  uint numCompanies;
+  mapping (uint => Companies) companies;
 
-  function newCampaign(string32 name, address wallet, uint goal) returns (uint campaignID) {
-    campaignID    = numCampaigns++;
-    Campaign cam  = campaigns[id];
-    cam.name      = name;
-    cam.creator   = msg.sender;
-    cam.wallet    = wallet;
-    cam.goal      = goal;
-    cam.isGoalMet = false;
+  function newCompany(string32 dapp, address wallet) returns (uint companyID) {
+    companyID    = numCompanies++;
+    Company com  = companies[companyID];
+    com.dapp     = dapp;
+    com.creator  = msg.sender;
+    com.wallet   = wallet;
   }
 
-  function invest(uint campaignID) returns (bool isInvested) {
-    Campaign cam = campaigns[campaignID];
-    if (cam.isGoalMet) {
-      return false;
-    }
-    Investor inv = cam.investors[cam.numInvestors++];
+  function invest(uint companyID) {
+    Company com  = companies[companyID];
+    Investor inv = com.investors[com.numInvestors++];
     inv.addr     = msg.sender;
     inv.amount   = msg.value;
-    cam.amount  += msg.value;
-    if (cam.amount >= cam.goal) {
-      cam.isGoalMet = true;
-    }
-    return  true;
+    com.amount  += msg.value;
   }
 
-  function isGoalMet(uint campaignID) returns (bool isGoalMet) {
-    return campaigns[campaignID].isGoalMet;
+  function getCompany(unit companyID) returns (Company) {
+    return companies[companyID];
+  }
+
+  function getInvestors(uint companyID) returns (mapping (uint=>Investor) investors) {
+    return campaigns[campaignID].investors;
   }
 
 }
